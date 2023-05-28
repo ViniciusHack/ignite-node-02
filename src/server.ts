ee9bc83ebbb4e0ee9c10985910dd38260e53dffb
vinicius <viniciusvwh1441@gmail.com>
2023-05-28 13:43:13 -0300

    test: create transaction test

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import cookie from '@fastify/cookie'
-import fastify from 'fastify'
-import { transactionRoutes } from './routes/transactions'
+import { app } from './app'
+import { env } from './env'
 
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionRoutes, {
-  prefix: 'transactions',
-})
-
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log(`> Ready on http://localhost:3333`)
 })
