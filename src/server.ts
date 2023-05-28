51b3bef03ddfbfe49f3d258832f8d61fca635e87
vinicius <viniciusvwh1441@gmail.com>
2023-05-28 14:45:54 -0300

    refactor: change server callback log

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log(`> Ready on http://localhost:3333`)
+  console.log(`> Ready on port ${env.PORT}`)
 })
